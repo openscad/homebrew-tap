e40285603f08ca4b597c46d6ada518ae61cba7ba
Marius Kintel <marius@kintel.net>
2016-06-25 23:19:09 -0400

    Test a clean install of opencsg

--- opencsg.rb
+++ opencsg.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Opencsg < Formula
   homepage 'http://www.opencsg.org'
-  url 'http://www.opencsg.org/OpenCSG-1.3.2.tar.gz'
-  sha1 'e2b4abf169ae3e319eb5f6d9ae6136fa96710a05'
+  url 'http://www.opencsg.org/OpenCSG-1.4.0.tar.gz'
+  sha1 '2c2592a9f625ec1c7a3d208403ea2ac1cae2f972'
 
   option "macosx-deployment-target=", "Mac OS X deployment target"
 
-  depends_on 'openscad/tap/qt' => :build
-  depends_on 'openscad/tap/glew'
+  depends_on 'qt5' => :build
+  depends_on 'glew'
 
   def install
     system "qmake", "-r", "PREFIX=#{prefix}",
@@ -17,46 +17,4 @@ class Opencsg < Formula
 
     system "make", "install"
   end
-
-  def patches
-    # Patch to make the library install correctly
-    DATA
-  end
 end
-
-__END__
-diff --git a/src/src.pro b/src/src.pro
-index 45aa1ea..71d201b 100644
---- a/src/src.pro
-+++ b/src/src.pro
-@@ -43,3 +43,16 @@ SOURCES		= area.cpp \
- 		  settings.cpp \
- 		  stencilManager.cpp \
- 		  ../RenderTexture/RenderTexture.cpp
-+
-+isEmpty(PREFIX) {
-+  PREFIX = /usr/local
-+}
-+
-+INCLUDEPATH += $$PREFIX/include
-+LIBS += -L$$PREFIX/lib -lGLEW
-+CONFIG += absolute_library_soname
-+
-+target.path = $$PREFIX/lib
-+headers.files = ../include/opencsg.h
-+headers.path = $$PREFIX/include
-+INSTALLS += target headers
-diff --git a/src/src.pro b/src/src.pro
-index 71d201b..67c6f20 100644
---- a/src/src.pro
-+++ b/src/src.pro
-@@ -4,7 +4,8 @@ VERSION     = 1.3.2
- DESTDIR     = ../lib
- 
- CONFIG		+= opengl warn_on release
--INCLUDEPATH += ../include ../glew/include ../
-+INCLUDEPATH += ../include ../
-+CONFIG -= qt
- 
- HEADERS		= ../include/opencsg.h \
- 		  opencsgConfig.h \
