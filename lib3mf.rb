a239eea1a1b6961431ae496b8fd3075915f0e2b6
Marius Kintel <marius@kintel.net>
2024-04-25 00:45:05 -0400

    Upgrade to lib3mf-2.3.1

--- lib3mf.rb
+++ lib3mf.rb
@@ -1,16 +1,16 @@
 class Lib3mf < Formula
   desc "Lib3MF - C++ implementation of the 3D Manufacturing Format file standard"
   homepage "https://github.com/3MFConsortium/lib3mf/"
-  url "https://github.com/3MFConsortium/lib3mf/archive/v1.8.1.tar.gz"
-  version "1.8.1"
-  sha256 "207dd142c9ca86a4fb1a4b2baadbdf579f35e03f9b8bf5c02dae027da5ae9d17"
+  url "https://github.com/3MFConsortium/lib3mf/archive/refs/tags/v2.3.1.tar.gz"
+  version "2.3.1"
+  sha256 "cdf6669f2aa0b7f3053cdf635b71f2cf34d98f9edb9b65432dde0d85c646ec71"
 
   depends_on "cmake" => :build
-  depends_on "ossp-uuid"
+  depends_on "libzip"
 
   def install
     mkdir "build" do
-      system "cmake", "-G", "Unix Makefiles", "-DLIB3MF_TESTS=false", "..", *std_cmake_args
+      system "cmake", "-G", "Unix Makefiles", "-DLIB3MF_TESTS=false", "-DUSE_INCLUDED_ZLIB=OFF", "-DUSE_INCLUDED_LIBZIP=OFF", "-DUSE_PLATFORM_UUID=ON", "..", *std_cmake_args
       system "make"
       system "make", "install"
     end
