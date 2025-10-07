825e27cadfc1fe5572af3985eb06c77faa4f8f38
Marius Kintel <marius@kintel.net>
2025-10-06 21:58:57 -0400

    Patch lib3mf to work with CMake 4.1

--- lib3mf.rb
+++ lib3mf.rb
@@ -20,13 +20,49 @@ class Lib3mf < Formula
 end
 __END__
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index c6a3179..00ae52d 100644
+index c6a3179..67a4b0f 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
 @@ -1,4 +1,4 @@
 -cmake_minimum_required (VERSION 3.0)
-+cmake_minimum_required (VERSION 3.5)
++cmake_minimum_required (VERSION 3.10)
  
  cmake_policy(SET CMP0054 NEW)
  cmake_policy(SET CMP0048 NEW)
-
+@@ -6,8 +6,6 @@ cmake_policy(SET CMP0048 NEW)
+ 
+ set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+ 
+-include(GNUInstallDirs)
+-
+ # Define Version
+ set(LIB3MF_VERSION_MAJOR 2)        # increase on every backward-compatibility breaking change of the API
+ set(LIB3MF_VERSION_MINOR 4)        # increase on every backward compatible change of the API
+@@ -19,15 +17,13 @@ project(lib3mf
+   DESCRIPTION "An implementation of the 3D Manufacturing Format file standard"
+ )
+ 
++include(GNUInstallDirs)
++
+ find_program(buildcache_program buildcache)
+ if(buildcache_program)
+   set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${buildcache_program}")
+ endif()
+ 
+-set(CMAKE_INSTALL_BINDIR bin CACHE PATH "directory for installing binary files")
+-set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "directory for installing library files")
+-set(CMAKE_INSTALL_INCLUDEDIR include/lib3mf CACHE PATH "directory for installing header files")
+-
+ option(USE_INCLUDED_ZLIB "Use included zlib" ON)
+ option(USE_INCLUDED_LIBZIP "Use included libzip" ON)
+ option(USE_INCLUDED_SSL "Use included libressl" ON)
+@@ -201,7 +197,8 @@ if (USE_INCLUDED_LIBZIP)
+ else()
+     find_package(PkgConfig REQUIRED)
+     pkg_check_modules(LIBZIP REQUIRED libzip)
+-    target_link_libraries(${PROJECT_NAME} ${LIBZIP_LIBRARIES})
++    target_link_libraries(${PROJECT_NAME} ${LIBZIP_LINK_LIBRARIES})
++    target_include_directories(${PROJECT_NAME} PRIVATE ${LIBZIP_INCLUDE_DIRS})
+ endif()
+ 
+ 
